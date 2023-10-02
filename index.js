c92d850e84f332f61fe8a3441febdd96b5ae5e32
Gleanna03 <sebgaletapia@gmail.com>
2023-10-02 17:42:28 +0800

    Added Code Exam

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 // TODO: rename this file from '.txt' to '.js'
 function isPalindrome(str) {
-  if (typeof str !== "string")
-    {
-      console.log(`The given value : ${str} in not a string! The expected data type of the value is  string.`);
-      return; 
-    }
-    const reverseStr = str.split("").reverse().join("");
+  if (typeof str !== "string"){
+    
+    console.log(`The given value : ${str} in not a string! The expected data type of the value is  string.`);
+    return; 
 
-    if(str === reverseStr){
-      console.log(`The given string: ${str} is an example of palindrome, the reverse value is: ${reverseStr}`);
-    }else {
-      console.log(`The given string: ${str} is not an example of palindrome`);
-    }
+  };
+
+  const reverseString = str.split("").reverse().join("");
+
+  if(str === reverseString){
+    console.log(`The given string: ${str} is an example of palindrome, the reverse value is: ${reverseString}`);
+  }else {
+    console.log(`The given string: ${str} is not an example of palindrome`);
+  };
+
 }
 
 
 
 // Call the function and pass an argument.
-isPalindrome("hello")
+isPalindrome("level")
